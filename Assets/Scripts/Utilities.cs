d7c6dfd11ad6f7c9eeb4540d299cde5789488075
Jose Manuel <josemanuelperezcarrasco.97@gmail.com>
2020-05-11 18:22:38 +0200

    Added enum to distinguish the different parts of the drum.

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities : MonoBehaviour {
+	public enum DrumParts {
+		RighCymbal = 0
+		,RightDrum = 1
+		,CentralDrum = 2
+		,LeftCymbal = 3
+		,LeftDrum = 4
+	}
+}
