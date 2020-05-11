d297db2f229957ac603146f0f7b2f2d6ad71da56
Jose Manuel <josemanuelperezcarrasco.97@gmail.com>
2020-05-11 12:40:54 +0200

    Added the main menu of the App and a credit scene.

---
+++ Assets/Scripts/CreditsMenuBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditsMenuBehaviour : MonoBehaviour {
+	private Button backButton;
+
+	// Use this for initialization
+	void Start () {
+		backButton = GameObject.Find("BackButton").GetComponent<Button>();
+		backButton.onClick.AddListener (delegate {
+			SceneManager.LoadScene ("mainMenu");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MainMenuBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehaviour : MonoBehaviour {
+	private Button playButton,playFree, creditsButton, exitButton;
+
+	// Use this for initialization
+	void Start () {
+		
+		//Button for showing the credits
+		creditsButton = GameObject.Find("Credits").GetComponent<Button>();
+		creditsButton.onClick.AddListener(
+			delegate{
+				SceneManager.LoadScene("creditsScene");
+			});
+
+		//Button for exiting the app
+		exitButton = GameObject.Find("Exit").GetComponent<Button>();
+		exitButton.onClick.AddListener(delegate{Application.Quit();});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
