ef4a28ae613cbab379b66932584d719909ed0e16
Jose Manuel <josemanuelperezcarrasco.97@gmail.com>
2020-06-03 19:32:16 +0200

    Added control to check if the drum set is plugged in before entering any mode in the main menu.

--- Assets/Scripts/MainMenuBehaviour.cs
+++ Assets/Scripts/MainMenuBehaviour.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO.Ports;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuBehaviour : MonoBehaviour {
 	private Button playButton,playFree, creditsButton, exitButton;
+	//Name of the Arduino port, to check if the port exists
+	public string portName = "COM3";
+	private bool existsPort = false;
+	//Game Objects related to the error panel
+	public GameObject errorPanel;
+	public Button errorButton;
 
 	// Use this for initialization
 	void Start () {
@@ -13,14 +21,24 @@ public class MainMenuBehaviour : MonoBehaviour {
 		creditsButton = GameObject.Find("GuidedMode").GetComponent<Button>();
 		creditsButton.onClick.AddListener(
 			delegate{
-				SceneManager.LoadScene("trackSelectionScene");
+				if(this.existsPort){
+					SceneManager.LoadScene("trackSelectionScene");
+				}
+				else{
+					this.errorPanel.SetActive (true);
+				}
 			});
 		
 		//Button to start the free mode of the App
 		creditsButton = GameObject.Find("FreeMode").GetComponent<Button>();
 		creditsButton.onClick.AddListener(
 			delegate{
-				SceneManager.LoadScene("freeModeScene");
+				if(this.existsPort){
+					SceneManager.LoadScene("freeModeScene");
+				}
+				else{
+					this.errorPanel.SetActive (true);
+				}
 			});
 		
 		//Button for showing the credits
@@ -30,6 +48,12 @@ public class MainMenuBehaviour : MonoBehaviour {
 				SceneManager.LoadScene("creditsScene");
 			});
 
+		//Button to close the error popup
+		this.errorButton.onClick.AddListener(
+			delegate{
+				this.errorPanel.SetActive (false);
+			});
+
 		//Button for exiting the app
 		exitButton = GameObject.Find("Exit").GetComponent<Button>();
 		exitButton.onClick.AddListener(delegate{Application.Quit();});
@@ -37,6 +61,7 @@ public class MainMenuBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//We update if the port exists or not
+		this.existsPort = Array.Exists(SerialPort.GetPortNames(), x => x == this.portName);
 	}
 }
